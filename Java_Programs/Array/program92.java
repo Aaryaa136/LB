edfd98b57525a606b8dbd422a863f6b8c3d84e71
Aaryaa136 <patilaaryaa85@gmail.com>
2026-04-11 12:45:16 +0530

    Add files via upload

---
+++ Java_Programs/Array/program92.java
@@ -0,0 +1,14 @@
+class program92
+{
+    public static void main(String A[])       //[]-subscript operator
+    {
+        int Arr[]={10,20,30,40};
+
+        System.out.println("Number of length:"+Arr.length);     //static memory allocation //4
+
+        System.out.println(Arr[0]);
+        System.out.println(Arr[1]);
+        System.out.println(Arr[2]);
+        System.out.println(Arr[3]);
+    }
+}
---
+++ Java_Programs/Array/program96.java
@@ -0,0 +1,14 @@
+class program96
+{
+    public static void main(String A[])       
+    {
+        int Arr[]=new int[4];
+
+        Arr[0]=10;
+        Arr[1]=20;
+        Arr[2]=30;
+        Arr[3]=40;
+
+        System.out.println("Number of length:"+Arr.length);     
+    }
+}
