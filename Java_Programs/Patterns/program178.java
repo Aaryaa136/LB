252d4cc84ad1ee7908ab0d44e51160b4ca9d58c4
Aaryaa136 <patilaaryaa85@gmail.com>
2026-06-15 17:50:08 +0530

    Add files via upload

---
+++ Java_Programs/Patterns/program178.java
@@ -0,0 +1,39 @@
+/* 
+    input:8
+    output: z   y   x   w   v   u   t   s
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iNo)
+    {
+        int iCnt=0;
+
+       char ch='z';
+
+        for(iCnt=1 ; iCnt<=iNo ; iCnt++)
+        {
+            System.out.printf("%c\t",ch);
+            ch--;
+        }
+        System.out.println();
+    }
+}
+
+class program178
+{
+    public static void main(String a[])
+    {
+        Scanner sobj=new Scanner(System.in);
+
+        int iValue=0;
+
+        System.out.println("Enter the frequency:");
+        iValue=sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue);
+    }
+}
